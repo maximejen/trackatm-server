6b65a8133a8e918b5cc2dc33ec3a4102d6397fbb
Maxime JENNY <maxime.jenny@epitech.eu>
2019-01-30 22:30:00 +0100

    Added OperationType + Post + Delete actions for Operation

--- src/AppBundle/Controller/Api/OperationController.php
+++ src/AppBundle/Controller/Api/OperationController.php
@@ -2,10 +2,13 @@
 namespace AppBundle\Controller\Api;
 
 use AppBundle\Entity\Operation;
+use AppBundle\Form\OperationType;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Serializer\SerializerInterface;
 
 /**
@@ -43,5 +46,56 @@ class OperationController extends Controller
     public function getOperationAction(Request $request, Operation $operation, SerializerInterface $serializer)
     {
         return new Response($serializer->serialize($operation, 'json', ['groups' => ['operation']]));
+    }
+
+    /**
+     * @Rest\View(statusCode=Response::HTTP_CREATED)
+     * @Rest\Post("/operations")
+     *
+     * @param Request $request
+     *
+     * @return Operation|\Symfony\Component\Form\FormInterface
+     *
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function postOperationsAction(Request $request)
+    {
+        $operation = new Operation();
+        $form = $this->createForm(OperationType::class, $operation);
+
+        $form->submit($request->request->all());
+
+        if ($form->isValid()) {
+            $em = $this->get('doctrine.orm.entity_manager');
+
+            $em->persist($operation);
+            $em->flush();
+
+            return $operation;
+        } else {
+            return $form;
+        }
+    }
+
+    /**
+     * @Rest\View(statusCode=Response::HTTP_NO_CONTENT)
+     * @Rest\Delete("/operations/{id}")
+     *
+     * @param Request $request
+     *
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function removeOperationAction(Request $request)
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+        /* @var $operation Operation */
+        $operation = $em
+            ->getRepository('AppBundle:Operation')
+            ->find($request->get('id'));
+
+        $em->remove($operation);
+        $em->flush();
     }
 }
