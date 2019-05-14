fe1445bf88fecf29e74057b0edd8b4330df5d15c
Maxime JENNY <maxime.jenny@epitech.eu>
2019-05-14 11:12:22 +0200

    moved a little the text

--- src/AppBundle/Controller/Api/OperationHistoryController.php
+++ src/AppBundle/Controller/Api/OperationHistoryController.php
@@ -159,14 +159,14 @@ class OperationHistoryController extends ApiController
 
         $watermark->setFontSize(35)
             ->setFont('Arial')
-            ->setOffset(0, 70)
+            ->setOffset(0, 75)
             ->setStyle(Watermark::STYLE_TEXT_DARK)
             ->setPosition(Watermark::POSITION_BOTTOM_RIGHT)
             ->setOpacity(1);
 
         $watermark1->setFontSize(35)
             ->setFont('Arial')
-            ->setOffset(0, 70)
+            ->setOffset(0, 75)
             ->setStyle(Watermark::STYLE_TEXT_DARK)
             ->setPosition(Watermark::POSITION_TOP_RIGHT)
             ->setOpacity(1);
